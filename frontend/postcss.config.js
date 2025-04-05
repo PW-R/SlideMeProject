2c4389e1263adcacd062371381e80f9fba53f40b
Pawares <pawarest.putkung@gmail.com>
2025-04-05 21:00:11 +0700

    'test'

--- frontend/postcss.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    plugins: [
-      require('tailwindcss'),  // Adds Tailwind CSS support
-      require('autoprefixer'),  // Automatically adds vendor prefixes (like -webkit-, -moz-)
-    ]
-  }
-  
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,9 @@ import './App.css'
 function App() {
 
   return (
-   <div>test</div>
+   <div>tdadadadadadadadadest</div>
+   
+<div>hhhhh</div>
   )
 }
 

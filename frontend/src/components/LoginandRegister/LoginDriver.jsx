ca40ae2d0f43587d044207254ee323fea54649ab
benchawan-bcw <benchawan.poo@spumail.net>
2025-04-09 20:21:35 +0700

    edit Route

--- frontend/src/components/LoginandRegister/LoginDriver.jsx
+++ frontend/src/components/LoginandRegister/LoginDriver.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 function LoginDriver() {
     return ( 
         <div>
-            <h1>ควยยยยยยยยยยยยย</h1>
+            <h1>Login driver</h1>
             <Link to="/HomeCustomize">
               <button>ไปฝั่งคนขับโว้ยยยยยยยย</button>
             </Link>
--- frontend/src/components/LoginandRegister/LoginUser.jsx
+++ frontend/src/components/LoginandRegister/LoginUser.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 function LoginUser() {
   return (
     <div>
-      <h1>ควยยยยยยยยยยยย</h1>
-      <Link to="/UserLayout/UserHome">
+      <h1>Login User</h1>
+      <Link to="/UserHome">
         <button>ไปฝั่งคนใช้โว้ยยยยยยยยย</button>
       </Link>
     </div>

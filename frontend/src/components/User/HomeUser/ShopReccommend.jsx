579f734d44d326b8f99ff8f2781889551e501ff5
Kanyarat2007 <kanyaratthinhanwong04@gmail.com>
2025-04-14 18:34:33 +0700

    แก้ลิ้ง

--- frontend/src/components/User/HomeUser/ShopReccommend.jsx
+++ frontend/src/components/User/HomeUser/ShopReccommend.jsx
@@ -1,41 +1,49 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function ShopReccommend() {
-    return (
-        <div>
-            {/* header */}
-            <div className="fixed w-[387px] shadow-[0_0_10px_#969696] bg-[#0dc964] h-[115px] flex items-end justify-center pb-2 rounded-b-3xl z-[3000]">
-                <Link to="/UserHome">
-                    <i className="bi bi-chevron-left mt-3 text-white text-2xl absolute left-3 bottom-4"></i>
-                </Link>
-                <h1 className="text-white">ร้านแนะนำ</h1>
-            </div>
+  const navigate = useNavigate();
 
-            <div className="pt-[140px] px-4 space-y-6">
-                <img
-                    src="./advert_car1.svg"
-                    alt="car1"
-                    className="w-full object-contain rounded-xl"
-                />
-                <img
-                    src="./advert_car2.svg"
-                    alt="car2"
-                    className="w-full object-contain rounded-xl"
-                />
-                <img
-                    src="./advert_car3.svg"
-                    alt="car3"
-                    className="w-full object-contain rounded-xl"
-                />
-                <img
-                    src="./advert_car4.svg"
-                    alt="car4"
-                    className="w-full object-contain rounded-xl"
-                />
-            </div>
-        </div>
-    );
+  // ฟังก์ชั่นสำหรับย้อนกลับ
+  const handleBack = () => {
+    navigate(-1);
+  };
+
+  return (
+    <div>
+      {/* header */}
+      <div className="fixed w-[387px] shadow-[0_0_10px_#969696] bg-[#0dc964] h-[115px] flex items-end justify-center pb-2 rounded-b-3xl z-[3000]">
+        <i
+          onClick={handleBack}
+          className="bi bi-chevron-left mt-3 text-white text-2xl absolute left-3 bottom-4 cursor-pointer"
+        ></i>
+        <h1 className="text-white">ร้านแนะนำ</h1>
+      </div>
+
+      <div className="pt-[140px] px-4 space-y-6">
+        <img
+          src="./advert_car1.svg"
+          alt="car1"
+          className="w-full object-contain rounded-xl"
+        />
+        <img
+          src="./advert_car2.svg"
+          alt="car2"
+          className="w-full object-contain rounded-xl"
+        />
+        <img
+          src="./advert_car3.svg"
+          alt="car3"
+          className="w-full object-contain rounded-xl"
+        />
+        <img
+          src="./advert_car4.svg"
+          alt="car4"
+          className="w-full object-contain rounded-xl"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default ShopReccommend;
